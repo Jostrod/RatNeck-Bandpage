3500623592c8da76cbcb12e1002053c6d5213777
Jostrod <jostroda@stud.ntnu.no>
2026-07-05 15:19:10 +0200

    feat: return field-level validation error messages

--- backend/src/main/java/no/ratneck/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/no/ratneck/backend/exception/GlobalExceptionHandler.java
@@ -4,10 +4,15 @@ package no.ratneck.backend.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,5 +25,20 @@ public class GlobalExceptionHandler {
                 HttpStatus.NOT_FOUND.getReasonPhrase(),
                 exception.getMessage(),
                 request.getRequestURI()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidData(MethodArgumentNotValidException methodArgumentNotValidException){
+        Map<String, String> response = new HashMap<>();
+
+        List<FieldError> errors = methodArgumentNotValidException.getBindingResult().getFieldErrors();
+
+        for (FieldError error:errors) {
+            String field = error.getField();
+            String message = error.getDefaultMessage();
+            response.put(field, message);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+
     }
 }
