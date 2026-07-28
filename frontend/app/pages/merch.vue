076df74bfdba5cb516b3d12a350fdb673ed705da
Jostrod <jostroda@stud.ntnu.no>
2026-07-28 17:56:33 +0200

    feat: mark sold-out merch products visually

--- frontend/app/pages/merch.vue
+++ frontend/app/pages/merch.vue
@@ -1,54 +1,50 @@
 <script setup lang="ts">
 
-    import type { Merch } from '~/types/merch';
+import type { Merch } from '~/types/merch';
 
-    const config = useRuntimeConfig()
-    const { status, data: merch } = await useFetch<Merch[]>(config.public.apiBase + '/merch')
+const config = useRuntimeConfig()
+const { status, data: merch } = await useFetch<Merch[]>(config.public.apiBase + '/merch')
 
-    
 
-    const formatCurrency = (value: number, locale = 'nb-NO') => {
-        
-        return new Intl.NumberFormat(locale, {
-            style: 'currency',
-            currency: 'NOK'
-        }).format(value)
-    }
+
+const formatCurrency = (value: number, locale = 'nb-NO') => {
+
+    return new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: 'NOK'
+    }).format(value)
+}
+
 
 </script>
 
 
 <template>
 
-<div>
+    <div>
 
-    <div v-if="status === 'error'">
-        Failed to load products
-    </div>
+        <div v-if="status === 'error'">
+            Failed to load products
+        </div>
 
-    <div v-else-if="status === 'pending'">
-        Loading products...
-    </div>
+        <div v-else-if="status === 'pending'">
+            Loading products...
+        </div>
 
-    <div v-else>
-        These are our products!
-        <div v-for="product in merch" 
-            :key="product.id">
+        <div v-else>
+            <div v-for="product in merch" :key="product.id">
 
-            <div>{{ product.merchType }}</div>
-            <div>{{ formatCurrency(product.price) }}</div>
-            <div>In stock: {{ product.inStock }}</div>
-            <div>{{ product.size }}</div>
-            <br>
-            
-            
-    
-    
-    </div>
+                <div>{{ product.merchType }}</div>
+                <div v-if="product.size">{{ product.size }}</div>
+                <div>{{ formatCurrency(product.price) }}</div>
+                <div v-if="!product.inStock" class="text-red-600 font-bold">SOLD OUT</div>
+                <br>
 
+            </div>
+
+
+        </div>
 
     </div>
-
-</div>
 
 </template>
