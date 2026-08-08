0029be933c0d8b18fac9ca278c9c069d2497106f
Jostrod <jostroda@stud.ntnu.no>
2026-08-08 12:40:09 +0200

    feat: add design tokens for colors and typography

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        "background-from": "#9d174d", 
+        "background-to": "#581c87",
+        "text-main": "#f5f5f5",
+        "primary": "#008000"
+      },
+      fontFamily: {
+        heading: ["Cinzel", "serif"],
+        body: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+};
