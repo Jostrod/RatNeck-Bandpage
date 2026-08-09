57e59d8678dece3948485e12ea8928374b6cb8d7
Jostrod <jostroda@stud.ntnu.no>
2026-08-09 19:58:32 +0200

    feat: add effects to NavLinks

--- frontend/app/components/NavLink.vue
+++ frontend/app/components/NavLink.vue
@@ -7,14 +7,16 @@ defineProps<{
 
 
 <template>
-    <NuxtLink :to="to" class="relative group"><slot />
+    <NuxtLink :to="to" class="p-2 mb-1 relative group hover:bg-black/10 transition duration-500 hover:scale-105 hover:translate-x-1 
+                            hover:-translate-y-0.5 hover:shadow-sm hover:shadow-black">
+                                    <slot />
         <!--Top line and right vertical line  -->
-        <span class="absolute right-0 top-0 w-0 h-[1px] bg-black transition-all duration-200 group-hover:w-full "></span>
-        <span class="absolute right-0 top-0 w-[1px] h-0 bg-black transition-all duration-200 group-hover:h-full"></span>
+        <span class="absolute right-0 top-0 w-0 h-[1px] bg-black transition-all duration-150 group-hover:w-full"></span>
+        <span class="absolute right-0 top-0 w-[1px] h-0 bg-black transition-all duration-150 group-hover:h-full"></span>
         
         <!--Bottom line and left vertical line -->
-        <span class="absolute left-0 bottom-0 w-0 h-[1px] bg-black transition-all duration-200 group-hover:w-full rounded-full"></span>
-        <span class="absolute left-0 bottom-0 w-[1px] h-0 bg-black transition-all duration-200 group-hover:h-full"></span>
+        <span class="absolute left-0 bottom-0 w-0 h-[1px] bg-black transition-all duration-150 group-hover:w-full rounded-full"></span>
+        <span class="absolute left-0 bottom-0 w-[1px] h-0 bg-black transition-all duration-150 group-hover:h-full"></span>
 
 
       </NuxtLink>
